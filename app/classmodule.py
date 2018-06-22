2a6d8dab3ff85aaac9ceb0e4c82fe7ef188d275c
rarhoade <rarhoade@umich.edu>
2018-06-22 14:04:46 -0400

    adding schools into information

--- app/classmodule.py
+++ app/classmodule.py
@@ -1,11 +1,21 @@
-class Information():  
+import random
+class Information():
+    total_loans = []
+    schools_list = [("Albion",27000), ("Andrews",17160), ("Kettering", 19385), ("Rochester", 10320)]
+    
+    def add_loan(self, in_loan):
+        total_loans.append(in_loan)
+        return;
+
     def __init__(self, name):
         self.name = name
+        self.school = random.choice(schools_list)
         self.grades = 4.0
         self.mental_health = 100
         self.health = 100 
         self.money = 5,000
-		
+
+    
     def downGrade(self,amount) :
         self.grades += amount
         #FIXME: calculate actual GPA and stuff here
