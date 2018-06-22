8b0ca46856262ad16f3ff1a5e5bcadbf6d95b632
rarhoade <rarhoade@umich.edu>
2018-06-22 13:48:51 -0400

    integrated beginning with __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,27 @@
 import sys
 from classmodule import Loan
+from classmodule import Information
 from funcmodule import my_function
+import os
+import random
+from time import sleep
+from driver import beginning_text 
+
 
 def main():
     args = sys.argv[1:]
     print('arg count :: {}'.format(len(args)))
     for arg in args:
         print('passed argument :: {}'.format(arg))
+    act_score = actGen()
+    person = beginning_text(act_score)
+    if act_score > 30:
+        #scholarship
+    else:
+        #super_debt
+
+    person_info = Information(person)    
+
 
 if __name__ == '__main__':
     main()
