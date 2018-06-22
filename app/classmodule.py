34ceb9f1be158688a4dc568790857a037a9dbd9c
Dakotah Feil <kotah@umich.edu>
2018-06-22 10:35:43 -0400

    added student class to app/classmodule

--- app/classmodule.py
+++ app/classmodule.py
@@ -1,6 +1,18 @@
-class MyClass():
-    def __init__(self, name):
+class Information():  
+	grades = 4.0
+	mental_health = 100
+	health = 100 
+	moneys = 5,000
+	
+	def __init__(self, name):
         self.name = name
-
-    def say_name(self):
-        print('name is {}'.format(self.name))
+		
+	def downGrade(decrement) :
+		grades = 4.0 - decrement
+		#calculate actual GPA and stuff here
+	
+	def downHealth(decrement_h,decrement_mh) :
+		health = health - decrement_h
+	
+	def downCash(decrement) :
+		moneys = moneys - decrement
